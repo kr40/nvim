89cb76ad863beb83b8a3e2646a468e27932d13ec
kr40 <kr40.git@gmail.com>
2023-12-18 16:11:33 +0530

    Changed tab behavior, check lua/plugins/supertab

--- lua/plugins/supertab.lua
+++ lua/plugins/supertab.lua
@@ -18,7 +18,8 @@ return {
 
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
+          if cmp.visible() and has_words_before() then
+            -- has_words_before makes sure that you don't enter cmp if you are only trying to indent
             -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
             cmp.select_next_item()
           -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
@@ -32,7 +33,7 @@ return {
           end
         end, { "i", "s" }),
         ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
+          if cmp.visible() and has_words_before() then
             cmp.select_prev_item()
           elseif luasnip.jumpable(-1) then
             luasnip.jump(-1)
