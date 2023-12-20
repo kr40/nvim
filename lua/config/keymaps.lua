3ea9e9a0e51f4ae5ef97614e2cead752899ccda7
kr40 <kr40.git@gmail.com>
2023-12-20 18:12:50 +0530

    Rearranged keymaps file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,25 +8,36 @@ local function opts(additional_options)
   return vim.tbl_extend("force", { noremap = true, silent = true }, additional_options)
 end
 
--- Function to rename all occurrences of the word under the cursor
-local rename = [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]
-
 -- Shorten vim.keymap to keymap
 local keymap = vim.keymap
+
+-- Function to rename all occurrences of the word under the cursor
+local rename = [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]
 
 -- Use J without moving the cursor
 keymap.set("n", "J", "mzJ`z")
 
--- Paste without losing the current register
+-- Paste without losing the current register in Visual modes
 keymap.set("x", "p", [["_dP]])
 
 -- Select all
 keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select All" })
 
+-- Rename under the cursor
+keymap.set("n", "<leader>zr", rename, opts({ desc = "Rename Under the Cursor" }))
+
+-- Run command and paste output
+keymap.set("n", "<leader>zp", [[:r!]], opts({ desc = "Run Command and Paste Output" }))
+
+-- Delete into the void register
+keymap.set({ "n", "x" }, "<leader>zd", [["_d]], opts({ desc = "Delete Into The Void Register" }))
+
 -- Make current file executable
 keymap.set("n", "<leader>zx", "<cmd>!chmod a+x %<CR>", opts({ desc = "Make Current File Executable" }))
 
--- Toggle Dashboard
+----------------------------------------------- Plugin Mappings -------------------------------------------------------
+
+-- Open Dashboard
 keymap.set("n", "<leader>;", "<cmd>Dashboard<CR>", opts({ desc = "Open Dashboard" }))
 
 -- Toggle Treesj
@@ -41,20 +52,11 @@ keymap.set("n", "<leader>zu", "<cmd>UndotreeToggle<CR>", opts({ desc = "Toggle U
 -- Toggle Barbecue
 keymap.set("n", "<leader>zb", "<cmd>Barbecue toggle<CR>", opts({ desc = "Toggle Barbecue" }))
 
--- Rename under the cursor
-keymap.set("n", "<leader>zr", rename, opts({ desc = "Rename All" }))
-
--- Run command and paste output
-keymap.set("n", "<leader>zp", [[:r!]], opts({ desc = "Run Command and Paste Output" }))
-
--- Delete current buffer into the void register
-keymap.set({ "n", "x" }, "<leader>zd", [["_d]], opts({ desc = "Delete into void register" }))
-
 -- Move mappings
 -- Move word left and right
 keymap.set("n", "<A-h>", ":MoveWord -1<CR>", opts({ desc = "Move Word Left" }))
 keymap.set("n", "<A-l>", ":MoveWord 1<CR>", opts({ desc = "Move Word Right" }))
 
--- Move Selection left and right
+-- Move block left and right
 keymap.set("v", "<A-h>", ":MoveHBlock -1<CR>", opts({ desc = "Move Block Left" }))
 keymap.set("v", "<A-l>", ":MoveHBlock 1<CR>", opts({ desc = "Move Block Right" }))
