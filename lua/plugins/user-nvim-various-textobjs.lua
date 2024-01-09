6560d47e7d2cd0795057e02e846289b9aed9eb93
kr40 <kr40.git@gmail.com>
2024-01-09 21:17:31 +0530

    Refactor added lazy to distro plugins, user to user plugins

--- lua/plugins/user-nvim-various-textobjs.lua
+++ lua/plugins/user-nvim-various-textobjs.lua
@@ -1,4 +1,5 @@
 return {
+  -- luacheck: ignore
   -- Adds text objects for various things (check - https://github.com/chrisgrieser/nvim-various-textobjs?tab=readme-ov-file)
   {
     "chrisgrieser/nvim-various-textobjs",
