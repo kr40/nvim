4ccbd525275f42751ca04439ceebb546b1e9b2cd
kr40 <kr40.git@gmail.com>
2024-01-16 18:17:31 +0530

    Commented unsupported syntax group in TokyoNight

--- lua/plugins/theme-tokyonight.lua
+++ lua/plugins/theme-tokyonight.lua
@@ -6,10 +6,10 @@ return {
         -- Style to be applied to different syntax groups
         -- Value is any valid attr-list value for `:help nvim_set_hl`
         comments = { italic = true },
-        conditionals = { italic = true },
         keywords = { italic = false },
         functions = { italic = true },
         variables = { italic = true },
+        -- conditionals = { italic = true },
         -- strings = { italic = true },
         -- numbers = { bold = true },
         -- boolean = { bold = true },
