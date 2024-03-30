e36340b51851f496f7b3875073d6d73913084a41
kr40 <kr40.git@gmail.com>
2024-03-30 13:37:52 +0530

    Removed dial and mini-move as it is available in extras

--- lua/plugins/user-outline.lua
+++ lua/plugins/user-outline.lua
@@ -2,17 +2,32 @@ return {
   -- Symbols Outline plugin for Neovim (Uses SPC cs)
   {
     "hedyhli/outline.nvim",
+    keys = { { "<leader>cS", "<cmd>Outline<cr>", desc = "Toggle Outline" } },
     cmd = "Outline",
-    keys = {
-      {
-        "<leader>cS",
-        "<cmd>Outline<CR>",
-        mode = "n",
-        noremap = true,
-        silent = true,
-        desc = "Toggle Symbols Outline",
-      },
-    },
-    opts = {},
+    opts = function()
+      local Config = require("lazyvim.config")
+      local defaults = require("outline.config").defaults
+      local opts = {
+        symbols = {},
+        symbol_blacklist = {},
+      }
+      local filter = Config.kind_filter
+
+      if type(filter) == "table" then
+        filter = filter.default
+        if type(filter) == "table" then
+          for kind, symbol in pairs(defaults.symbols) do
+            opts.symbols[kind] = {
+              icon = Config.icons.kinds[kind] or symbol.icon,
+              hl = symbol.hl,
+            }
+            if not vim.tbl_contains(filter, kind) then
+              table.insert(opts.symbol_blacklist, kind)
+            end
+          end
+        end
+      end
+      return opts
+    end,
   },
 }
